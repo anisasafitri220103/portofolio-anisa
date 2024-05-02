815948ab17a3339fd22f58074d27a7306a17dcc0
anisasafitri220103~ <anisasafitri21076005@gmail.com>
2024-05-03 01:28:49 +0700

    website-portofolio-anisa

---
+++ app/Http/Controllers/skillController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\metadata;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+
+class skillController extends Controller
+{
+    public function index()
+    {
+        $skill_url = public_path('admin/devicon.json');
+        $skill_data = file_get_contents($skill_url);
+        $skill_data = json_decode($skill_data, true);
+        $skill = array_column($skill_data, 'name');
+        $skill = "'".implode("','", $skill)."'";
+
+        return view('dashboard.skill.index')->with(['skill'=>$skill]);
+    }
+    public function update(Request $request)
+    {
+        if($request->method() == 'POST'){
+            $request->validate([
+                '_language'=> 'required',
+                '_workflow'=> 'required'
+            ],[
+                '_language.required' => 'Silakan Masukkan Bahasa Pemrogramman Yang Anda Kuasai',
+                '_workflow.required' => 'Silakan Masukkan Workflow Yang Anda Kuasai',
+            ]);
+
+            metadata::updateOrCreate(['meta_key' => '_language'], ['meta_value' => $request->_language]);
+            metadata::updateOrCreate(['meta_key' => '_workflow'], ['meta_value' => $request->_workflow]);
+
+            return redirect()->route('skill.index')->with('success', 'Berhasil Melakukan Update Data Skill.');
+
+        }
+    }
+}
